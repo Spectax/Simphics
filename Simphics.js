cbec04648d170f04615375c8cb1e9a6cfbbcb001
Sai Teja Jammalamadaka <tsjamm@gmail.com>
2013-02-17 11:32:55 +0530

    Added Dimensional Particles

--- Simphics.js
+++ Simphics.js
@@ -9,3 +9,40 @@ function acceleration(v1,v2,t1,t2)
 {
 	return (v2-v1)/(t2-t1);
 }
+
+
+// An object representing a simple partical that moves in a single direction
+function oneDimensionalParticle(x,v,a)
+{
+	this.positionX = x;
+	this.velocityX = v;
+	this.accelerationX = a;
+}
+
+// An object representing a simple partical that moves in 2 directions
+function twoDimensionalParticle(x,y,vx,vy,ax,ay)
+{
+	this.positionX = x;
+	this.velocityX = vx;
+	this.accelerationX = ax;
+
+	this.positionY = y;
+	this.velocityY = vy;
+	this.accelerationY = ay;
+}
+
+// An object representing a simple partical that moves in 3 directions
+function threeDimensionalParticle(x,y,z,vx,vy,vz,ax,ay,az)
+{
+	this.positionX = x;
+	this.velocityX = v;
+	this.accelerationX = a;
+
+	this.positionY = y;
+	this.velocityY = vy;
+	this.accelerationY = ay;
+
+	this.positionZ = z;
+	this.velocityZ = vz;
+	this.accelerationZ = az;
+}
