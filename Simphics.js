92dc578047af8d4cdc33f4d5d76b30e1426006ec
Sai Teja Jammalamadaka <tsjamm@gmail.com>
2013-02-17 13:28:18 +0530

    Created Position, Velocity, Acceleration Objects

--- Simphics.js
+++ Simphics.js
@@ -1,48 +1,87 @@
 // Simphics - A Physics Library in Javascript
 
-function velocity(x1,x2,t1,t2)
+
+// Position Object
+function Position(x,y,z)
 {
-	return (x2-x1)/(t2-t1);
+	this.positionX= x;
+	this.positionY= y;
+	this.positionZ= z;
+	
+	this.x = function(){return this.positionX;}
+	this.y = function(){return this.positionY;}
+	this.z = function(){return this.positionZ;}
+
+	this.distanceFromOrigin = function(){return Math.sqrt(this.x*this.x+this.y*this.y+this.z*this.z);}
 }
 
-function acceleration(v1,v2,t1,t2)
+// Velocity Object
+function Velocity(vx,vy,vz)
 {
-	return (v2-v1)/(t2-t1);
+	this.velocityX= vx;
+	this.velocityY= vy;
+	this.velocityZ= vz;
+	
+	this.vX = function(){return this.velocityX;}
+	this.vY = function(){return this.velocityY;}
+	this.vZ = function(){return this.velocityZ;}
+
+	this.magnitude = function(){return Math.sqrt(this.vX()*this.vX() + this.vY()*this.vY() + this.vZ()*this.vZ())}
+}
+
+// Acceleration Object
+function Acceleration(ax,ay,az)
+{
+	this.accelerationX= ax;
+	this.accelerationY= ay;
+	this.accelerationZ= az;
+	
+	this.aX = function(){return this.accelerationX;}
+	this.aY = function(){return this.accelerationY;}
+	this.aZ = function(){return this.accelerationZ;}
+
+	this.magnitude = function(){return Math.sqrt(this.aX()*this.aX() + this.aY()*this.aY() + this.aZ()*this.aZ())}
 }
 
 
-// An object representing a simple partical that moves in a single direction
-function oneDimensionalParticle(x,v,a)
+// An object representing a simple particle that moves in 3 directions
+// Takes as input a position object, velocity object, and acceleration object
+function Particle(position,velocity,acceleration)
 {
-	this.positionX = x;
-	this.velocityX = v;
-	this.accelerationX = a;
-}
+	this.positionX = position.x();
+	this.velocityX = velocity.vX();
+	this.accelerationX = acceleration.aX();
 
-// An object representing a simple partical that moves in 2 directions
-function twoDimensionalParticle(x,y,vx,vy,ax,ay)
-{
-	this.positionX = x;
-	this.velocityX = vx;
-	this.accelerationX = ax;
+	this.positionY = position.y();
+	this.velocityY = velocity.vY();
+	this.accelerationY = acceleration.aY();
 
-	this.positionY = y;
-	this.velocityY = vy;
-	this.accelerationY = ay;
-}
+	this.positionZ = position.z();
+	this.velocityZ = velocity.vZ();
+	this.accelerationZ = acceleration.aZ();
 
-// An object representing a simple partical that moves in 3 directions
-function threeDimensionalParticle(x,y,z,vx,vy,vz,ax,ay,az)
-{
-	this.positionX = x;
-	this.velocityX = v;
-	this.accelerationX = a;
+	this.resultantVelocity = velocity.magnitude();
+	this.resultantAcceleration = acceleration.magnitude();
 
-	this.positionY = y;
-	this.velocityY = vy;
-	this.accelerationY = ay;
+	this.x = function(){return this.positionX;}
+	this.y = function(){return this.positionY;}
+	this.z = function(){return this.positionZ;}
+	this.vX = function(){return this.velocityX;}
+	this.vY = function(){return this.velocityY;}
+	this.vZ = function(){return this.velocityZ;}
+	this.aX = function(){return this.accelerationX;}
+	this.aY = function(){return this.accelerationY;}
+	this.aZ = function(){return this.accelerationZ;}
 
-	this.positionZ = z;
-	this.velocityZ = vz;
-	this.accelerationZ = az;
+	this.v = function(){return this.resultantVelocity;}
+	this.a = function(){return this.resultantAcceleration;}
+
+	this.infoString = function()
+	{
+		var infoPosition = 'Position is ('+this.x()+','+this.y()+','+this.z()+')\n';
+		var infoVelocity = 'Velocity is ('+this.vX()+','+this.vY()+','+this.vZ()+') = '+this.v()+'\n';
+		var infoAcceleration = 'Acceleration is ('+this.aX()+','+this.aY()+','+this.aZ()+') = '+this.a()+'\n';
+		
+		return infoPosition+infoVelocity+infoAcceleration;
+	}
 }
