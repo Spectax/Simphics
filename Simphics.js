c41cc4bb9eee8d9fec8886ee29ebe11a186d9ed0
Sai Teja Jammalamadaka <tsjamm@gmail.com>
2013-02-17 14:07:48 +0530

    Added License

--- Simphics.js
+++ Simphics.js
@@ -48,40 +48,39 @@ function Acceleration(ax,ay,az)
 // Takes as input a position object, velocity object, and acceleration object
 function Particle(position,velocity,acceleration)
 {
-	this.positionX = position.x();
-	this.velocityX = velocity.vX();
-	this.accelerationX = acceleration.aX();
+	this.position = position;
+	this.velocity = velocity;
+	this.acceleration = acceleration;
 
-	this.positionY = position.y();
-	this.velocityY = velocity.vY();
-	this.accelerationY = acceleration.aY();
+	this.x = function(){return this.position.x();}
+	this.y = function(){return this.position.y();}
+	this.z = function(){return this.position.z();}
+	this.vX = function(){return this.velocity.vX();}
+	this.vY = function(){return this.velocity.vY();}
+	this.vZ = function(){return this.velocity.vZ();}
+	this.aX = function(){return this.acceleration.aX();}
+	this.aY = function(){return this.acceleration.aY();}
+	this.aZ = function(){return this.acceleration.aZ();}
 
-	this.positionZ = position.z();
-	this.velocityZ = velocity.vZ();
-	this.accelerationZ = acceleration.aZ();
-
-	this.resultantVelocity = velocity.magnitude();
-	this.resultantAcceleration = acceleration.magnitude();
-
-	this.x = function(){return this.positionX;}
-	this.y = function(){return this.positionY;}
-	this.z = function(){return this.positionZ;}
-	this.vX = function(){return this.velocityX;}
-	this.vY = function(){return this.velocityY;}
-	this.vZ = function(){return this.velocityZ;}
-	this.aX = function(){return this.accelerationX;}
-	this.aY = function(){return this.accelerationY;}
-	this.aZ = function(){return this.accelerationZ;}
-
-	this.v = function(){return this.resultantVelocity;}
-	this.a = function(){return this.resultantAcceleration;}
+	this.d = function(){return this.position.distanceFromOrigin();}
+	this.v = function(){return this.velocity.magnitude();}
+	this.a = function(){return this.acceleration.magnitude();}
 
 	this.infoString = function()
 	{
-		var infoPosition = 'Position is ('+this.x()+','+this.y()+','+this.z()+')\n';
+		var infoPosition = 'Position is ('+this.x()+','+this.y()+','+this.z()+') = '+this.d+' from Origin\n';
 		var infoVelocity = 'Velocity is ('+this.vX()+','+this.vY()+','+this.vZ()+') = '+this.v()+'\n';
 		var infoAcceleration = 'Acceleration is ('+this.aX()+','+this.aY()+','+this.aZ()+') = '+this.a()+'\n';
 		
 		return infoPosition+infoVelocity+infoAcceleration;
+	}
+}
+
+// Relative Object takes two objects and gives the relative properties of object 1 wrt object 2
+// (ie, perspective of Object 1 from Object 2)
+function Relative(Object1, Object2)
+{
+	this.relativePosition = function() {
+		
 	}
 }
